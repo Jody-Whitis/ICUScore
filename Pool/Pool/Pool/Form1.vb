b4dc1dd816a0a22df13539d726d1db6486ed8f76
Jody Whitis <jodywhitis0407@gmail.com>
2019-07-01 09:33:10 -0500

    Adding display wins logic
    Displays wins if none, otherwise display current total wins. Inserts wins if saved again with number of wins, will add sql and logic to increment by one win per game.

---
+++ Pool/Pool/Pool/Form1.vb
@@ -0,0 +1,259 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1
+
+    Dim connectionString As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jwhitis\source\repos\Pool\Pool\LocalResults.mdf;Integrated Security=True"
+    Dim player1Set As Boolean = False
+    Dim player2Set As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LocalResultsDataSet1.Players' table. You can move, or remove it, as needed.
+        Me.PlayersTableAdapter1.Fill(Me.LocalResultsDataSet1.Players)
+        'TODO: This line of code loads data into the 'PlayerNames.Players' table. You can move, or remove it, as needed.
+        Me.PlayersTableAdapter.Fill(Me.PlayerNames.Players)
+        Dim player1 As New PlayerStats
+        Dim player2 As New PlayerStats
+        Dim lstPlayers As New List(Of PlayerStats)
+        Dim results As String = String.Empty
+
+        lblError.Visible = False
+        groupWinner.Visible = False
+        player1.PlayerName1 = tbPlayer1.ToString
+        player2.PlayerName1 = tbPlayer2.ToString
+
+
+        With lstPlayers
+            .Add(player1)
+            .Add(player2)
+        End With
+
+
+
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim player1 As String = String.Empty
+        Dim wins As Integer = 0
+        Dim command As String = "exec [dbo].[selPlayers_v1.1]"
+        Dim adapter As New SqlDataAdapter
+        Dim ds As New DataSet
+        Dim dbCommand As New SqlCommand
+        Dim sqlConnection As New SqlConnection
+
+        tbPlayer1.Visible = False
+        tbPlayer2.Visible = False
+        lblError.Visible = False
+
+
+
+        If Not String.IsNullOrEmpty(cbPlayer1.SelectedValue.ToString) And Not String.IsNullOrEmpty(cbPlayer2.SelectedValue.ToString) Then
+
+#Region "if wins are not empty then add new game, else display wins"
+            If Not String.IsNullOrEmpty(txtWins.Text) AndAlso Not String.IsNullOrEmpty(txtWins2.Text) Then
+
+                With sqlConnection
+                    .ConnectionString = connectionString
+                    .Open()
+                End With
+
+                adapter = New SqlDataAdapter("", sqlConnection)
+                adapter.Fill(ds)
+
+
+                Try
+                    txtWins.Text = ds.Tables(0).Rows(0).Item(0).ToString
+                    txtWins2.Text = ds.Tables(0).Rows(1).Item(0).ToString
+
+
+                Catch ex As Exception
+                    sqlConnection.Close()
+                    lblError.Text = "Game not saved"
+                    lblError.Visible = True
+                Finally
+                    sqlConnection.Close()
+
+                End Try
+
+
+            Else 'we'll open current wins
+                With sqlConnection
+                    .ConnectionString = connectionString
+                    .Open()
+                End With
+
+                adapter = New SqlDataAdapter("select wins from Players where playerName in ('" & cbPlayer1.SelectedValue.ToString & "','" & cbPlayer2.SelectedValue.ToString & "')", sqlConnection)
+                adapter.Fill(ds)
+
+                'save new wins if null txtbox wins, else then insert new game results.
+                Try
+                    txtWins.Text = ds.Tables(0).Rows(0).Item(0).ToString
+                    txtWins2.Text = ds.Tables(0).Rows(1).Item(0).ToString
+
+
+                Catch ex As Exception
+                    sqlConnection.Close()
+                    lblError.Text = "Name not found"
+                    lblError.Visible = True
+                Finally
+                    sqlConnection.Close()
+
+                End Try
+
+            End If
+
+#End Region
+
+
+        End If
+
+
+
+
+    End Sub
+
+
+
+
+    Private Sub cbPlayer1Win_CheckedChanged(sender As Object, e As EventArgs) Handles cbPlayer1Win.CheckedChanged
+
+    End Sub
+
+    Private Sub btnReg_Click(sender As Object, e As EventArgs) Handles btnReg.Click
+        Dim player1 As String = String.Empty
+        Dim wins As Integer = 0
+        Dim command As String = "exec insNewPlayer @newPlayer='" & tbPlayer1.Text & "'exec insNewPlayer @newPlayer='" & tbPlayer2.Text & "'"
+
+
+        Dim dbCommand As New SqlCommand
+        Dim sqlConnection As New SqlConnection
+
+        tbPlayer1.Visible = True
+        tbPlayer2.Visible = True
+
+
+        'If players have names, then update cbox and add them to db
+        If Not String.IsNullOrEmpty(tbPlayer1.Text) And Not String.IsNullOrEmpty(tbPlayer2.Text) Then
+
+            With sqlConnection
+                .ConnectionString = connectionString
+                .Open()
+            End With
+
+            cbPlayer1.BeginUpdate()
+            cbPlayer2.BeginUpdate()
+            With dbCommand
+                .Connection = sqlConnection
+                .CommandText = command
+                .ExecuteNonQuery()
+
+            End With
+            cbPlayer1.EndUpdate()
+            cbPlayer2.EndUpdate()
+
+            Try
+
+                'If ds.Tables(0).Rows(0).Item("player1win") / ds.Tables(0).Rows(0).Item("player2win") * 100 >= 50 Or ds.Tables(0).Rows(0).Item("player2win") / ds.Tables(0).Rows(0).Item("player1win") * 100 >= 50 Then
+                '    lblError.Text = "RIVALARY!!!"
+                '    lblError.Visible = True
+
+                'End If
+
+
+            Catch ex As Exception
+                sqlConnection.Close()
+                lblError.Text = "Name not found"
+                lblError.Visible = True
+            Finally
+                sqlConnection.Close()
+
+            End Try
+
+
+            lblError.Text = "New Players Added"
+            lblError.Visible = True
+            tbPlayer1.ResetText()
+            tbPlayer2.ResetText()
+            cbPlayer1.Visible = True
+            cbPlayer2.Visible = True
+
+#Region "logic for future rivalry stats"
+            'With sqlConnection
+            '    .ConnectionString = connectionString
+            '    .Open()
+            'End With
+
+
+
+
+            'adapter = New SqlDataAdapter("select player1win,player2win,player1name,player2name from games where player1name like '" & tbPlayer1.Text & "'" & "and player2name like '" & tbPlayer2.Text & "'", sqlConnection)
+            'adapter.Fill(ds)
+
+
+            'Try
+
+            '    If ds.Tables(0).Rows(0).Item("player1win") / ds.Tables(0).Rows(0).Item("player2win") * 100 >= 50 Or ds.Tables(0).Rows(0).Item("player2win") / ds.Tables(0).Rows(0).Item("player1win") * 100 >= 50 Then
+            '        lblError.Text = "RIVALARY!!!"
+            '        lblError.Visible = True
+
+            '    End If
+            'Catch ex As Exception
+            '    sqlConnection.Close()
+            '    lblError.Text = "Name not found"
+            '    lblError.Visible = True
+            'Finally
+            '    sqlConnection.Close()
+
+            'End Try
+
+            'if no players, then turn on tboxes to added them
+#End Region
+
+        Else
+
+            tbPlayer1.Visible = True
+                tbPlayer2.Visible = True
+
+            cbPlayer1.Visible = False
+            cbPlayer2.Visible = False
+        End If
+
+
+    End Sub
+
+    Private Sub FillByToolStripButton_Click(sender As Object, e As EventArgs)
+        Try
+            Me.PlayersTableAdapter.FillBy(Me.PlayerNames.Players)
+        Catch ex As System.Exception
+            System.Windows.Forms.MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub FillBy1ToolStripButton_Click(sender As Object, e As EventArgs)
+        Try
+            Me.PlayersTableAdapter.FillBy1(Me.PlayerNames.Players)
+        Catch ex As System.Exception
+            System.Windows.Forms.MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub cbPlayer1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbPlayer1.SelectedIndexChanged
+        player1Set = True
+    End Sub
+
+    Private Sub FillByToolStripButton_Click_1(sender As Object, e As EventArgs)
+        Try
+            Me.PlayersTableAdapter1.FillBy(Me.LocalResultsDataSet1.Players)
+        Catch ex As System.Exception
+            System.Windows.Forms.MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub cbPlayer2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbPlayer2.SelectedIndexChanged
+        player2Set = True
+    End Sub
+End Class
---
+++ Pool/Pool/Pool/PlayerStats.vb
@@ -0,0 +1,62 @@
+﻿Public Class PlayerStats
+    Private PlayerName As String
+    Private Wins As Int32
+    Private Loses As Int32
+    Private lastOp As String
+    Private Results As String
+    Private winsAgainst As List(Of String)
+
+    Public Property PlayerName1 As String
+        Get
+            Return PlayerName
+        End Get
+        Set(value As String)
+            PlayerName = value
+        End Set
+    End Property
+
+    Public Property Wins1 As Integer
+        Get
+            Return Wins
+        End Get
+        Set(value As Integer)
+            Wins = value
+        End Set
+    End Property
+
+    Public Property Loses1 As Integer
+        Get
+            Return Loses
+        End Get
+        Set(value As Integer)
+            Loses = value
+        End Set
+    End Property
+
+    Public Property LastOp1 As String
+        Get
+            Return lastOp
+        End Get
+        Set(value As String)
+            lastOp = value
+        End Set
+    End Property
+
+    Public Property Results1 As String
+        Get
+            Return Results
+        End Get
+        Set(value As String)
+            Results = value
+        End Set
+    End Property
+
+    Public Property WinsAgainst1 As List(Of String)
+        Get
+            Return winsAgainst
+        End Get
+        Set(value As List(Of String))
+            winsAgainst = value
+        End Set
+    End Property
+End Class
