5b862177bc021cccd0c9d2cfceae07b504308006
Jody Whitis <jodywhitis0407@gmail.com>
2019-09-03 16:42:35 -0500

    Adding proj for interface
    Folder
    project
    classes
    dependancies

---
+++ PoolDBConnect/IDBConnect.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows.Forms
+
+Public Interface IDBConnect
+
+
+    'connect
+
+
+    'insert
+    Sub InsertGame()
+    Sub InsertPlayer()
+
+    'pull up
+    Sub GetGames(cb As ComboBox, cb2 As ComboBox, txtwins As TextBox, txtWins2 As TextBox, lblError As Label)
+    Sub GetPlayers(cbPlayer1 As ComboBox, txtwins As TextBox, lblError As Label)
+    Sub getResults(tb As TextBox)
+
+
+    'close
+
+
+
+End Interface
