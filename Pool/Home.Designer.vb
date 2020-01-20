e043067a2299186746f5653e2aa042ff11df9f8d
Jody <jwhitis@JODYW-007041291>
2020-01-20 16:15:32 -0600

    Implementing current User's login / logout at home form.

--- Pool/Home.Designer.vb
+++ Pool/Home.Designer.vb
@@ -26,34 +26,35 @@ Partial Class Home
         Me.btnHS = New System.Windows.Forms.Button()
         Me.btnQuit = New System.Windows.Forms.Button()
         Me.lblHome = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnLogout = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnPvP
         '
-        Me.btnPvP.Location = New System.Drawing.Point(83, 131)
-        Me.btnPvP.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnPvP.Location = New System.Drawing.Point(124, 205)
         Me.btnPvP.Name = "btnPvP"
-        Me.btnPvP.Size = New System.Drawing.Size(131, 49)
+        Me.btnPvP.Size = New System.Drawing.Size(196, 77)
         Me.btnPvP.TabIndex = 0
         Me.btnPvP.Text = "PVP"
         Me.btnPvP.UseVisualStyleBackColor = True
         '
         'btnHS
         '
-        Me.btnHS.Location = New System.Drawing.Point(327, 131)
-        Me.btnHS.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnHS.Location = New System.Drawing.Point(490, 205)
         Me.btnHS.Name = "btnHS"
-        Me.btnHS.Size = New System.Drawing.Size(131, 49)
+        Me.btnHS.Size = New System.Drawing.Size(196, 77)
         Me.btnHS.TabIndex = 1
         Me.btnHS.Text = "HS"
         Me.btnHS.UseVisualStyleBackColor = True
         '
         'btnQuit
         '
-        Me.btnQuit.Location = New System.Drawing.Point(201, 231)
-        Me.btnQuit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnQuit.Location = New System.Drawing.Point(490, 361)
         Me.btnQuit.Name = "btnQuit"
-        Me.btnQuit.Size = New System.Drawing.Size(131, 49)
+        Me.btnQuit.Size = New System.Drawing.Size(196, 77)
         Me.btnQuit.TabIndex = 2
         Me.btnQuit.Text = "Quit"
         Me.btnQuit.UseVisualStyleBackColor = True
@@ -61,22 +62,59 @@ Partial Class Home
         'lblHome
         '
         Me.lblHome.AutoSize = True
-        Me.lblHome.Location = New System.Drawing.Point(146, 40)
+        Me.lblHome.Location = New System.Drawing.Point(219, 62)
+        Me.lblHome.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblHome.Name = "lblHome"
-        Me.lblHome.Size = New System.Drawing.Size(124, 17)
+        Me.lblHome.Size = New System.Drawing.Size(188, 25)
         Me.lblHome.TabIndex = 3
         Me.lblHome.Text = "Pick a Game Type"
         '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(241, 99)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(325, 31)
+        Me.txtUser.TabIndex = 4
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(241, 151)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPassword.Size = New System.Drawing.Size(325, 31)
+        Me.txtPassword.TabIndex = 5
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(326, 207)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(140, 75)
+        Me.btnLogin.TabIndex = 6
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'btnLogout
+        '
+        Me.btnLogout.Location = New System.Drawing.Point(124, 361)
+        Me.btnLogout.Name = "btnLogout"
+        Me.btnLogout.Size = New System.Drawing.Size(196, 75)
+        Me.btnLogout.TabIndex = 7
+        Me.btnLogout.Text = "Log Out"
+        Me.btnLogout.UseVisualStyleBackColor = True
+        '
         'Home
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(533, 288)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnLogout)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUser)
         Me.Controls.Add(Me.lblHome)
         Me.Controls.Add(Me.btnQuit)
         Me.Controls.Add(Me.btnHS)
         Me.Controls.Add(Me.btnPvP)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Home"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -88,4 +126,8 @@ Partial Class Home
     Friend WithEvents btnHS As Button
     Friend WithEvents btnQuit As Button
     Friend WithEvents lblHome As Label
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents btnLogout As Button
 End Class
--- Pool/Home.vb
+++ Pool/Home.vb
@@ -7,22 +7,13 @@ Public Class Home
         Me.CenterToScreen()
         homeTheme.Screen = AppState.Start
         homeTheme.SetBackground(Me)
-        homeTheme.SetButtons(New Button() {btnPvP, btnHS, btnQuit})
+        homeTheme.SetButtons(New Button() {btnPvP, btnHS, btnQuit, btnLogin, btnLogout})
+        homeTheme.SetVisibiltyButton(New Button() {btnPvP, btnHS, btnLogout}, False)
+        homeTheme.SetVisibiltyButton(New Button() {btnLogin, btnQuit}, True)
         lblHome.ForeColor = Color.Aquamarine
         lblHome.Font = New Font("Gill Sans Ultra", 15, FontStyle.Bold)
+        lblHome.Visible = False
         Me.ShowIcon = True
-        Dim test As New Authenticate
-        With test
-            .User = "jodywhitis0407@gmail.com"
-            .Password = "password123"
-        End With
-        Dim loggedIn As Boolean = test.GetLogin()
-        test.isLoggedIn = loggedIn
-        If loggedIn.Equals(True) Then
-            UserMod.UserEmail = test.User
-            UserMod.IsLoggedIn = test.isLoggedIn
-            'test.ILogin_UpdatePassword("123abc")
-        End If
     End Sub
 
     Private Sub btnPvP_Click(sender As Object, e As EventArgs) Handles btnPvP.Click
@@ -39,5 +30,38 @@ Public Class Home
 
     Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
         Application.Exit()
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim userAuthenticate As New Authenticate
+        With userAuthenticate
+            .User = txtUser.Text.ToString
+            .Password = txtPassword.Text.ToString
+            'password123
+        End With
+        Dim loggedIn As Boolean = userAuthenticate.GetLogin()
+        userAuthenticate.isLoggedIn = loggedIn
+        If loggedIn.Equals(True) Then
+            UserMod.UserEmail = userAuthenticate.User
+            UserMod.IsLoggedIn = userAuthenticate.isLoggedIn
+            ' userAuthenticate.ILogin_UpdatePassword("123abc") 
+            homeTheme.SetVisibiltyButton(New Button() {btnHS, btnPvP, btnLogout}, True)
+            homeTheme.SetVisibiltyButton(New Button() {btnLogin}, False)
+            homeTheme.SetVisiblityTxtBox(New TextBox() {txtUser, txtPassword}, False)
+            lblHome.Visible = True
+        Else
+            Dim incorrectAlert As DialogResult = MessageBox.Show($"Incorrect Email and/or/also/maybe Password",
+    "Incorrect Creditials", MessageBoxButtons.OK, MessageBoxIcon.Hand)
+        End If
+        txtPassword.ResetText()
+    End Sub
+
+    Private Sub btnLogout_Click(sender As Object, e As EventArgs) Handles btnLogout.Click
+        UserMod.UserEmail = String.Empty
+        UserMod.IsLoggedIn = False
+        homeTheme.SetVisibiltyButton(New Button() {btnHS, btnPvP, btnLogout}, False)
+        homeTheme.SetVisibiltyButton(New Button() {btnLogin}, True)
+        homeTheme.SetVisiblityTxtBox(New TextBox() {txtUser, txtPassword}, True)
+        lblHome.Visible = False
     End Sub
 End Class
