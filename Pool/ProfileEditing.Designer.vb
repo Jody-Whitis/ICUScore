bf4ea6d06862cad5a47dc9c0c897bbdae4a210e6
JodyWhitis <jody.whitis01@utrgv.edu>
2020-04-19 00:06:59 -0500

    Adding logic for comparing initial and new saved values. Adding store procedure params check to only update and pass values that were changed. Added prompts on nothing changed and save confirmation. Hidden the deactivate checkbox.

--- Pool/ProfileEditing.Designer.vb
+++ Pool/ProfileEditing.Designer.vb
@@ -36,9 +36,10 @@ Partial Class ProfileEditing
         Me.cbTwoFactorEnabled.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 10.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cbTwoFactorEnabled.ForeColor = System.Drawing.Color.Aquamarine
         Me.cbTwoFactorEnabled.ImageAlign = System.Drawing.ContentAlignment.TopLeft
-        Me.cbTwoFactorEnabled.Location = New System.Drawing.Point(69, 379)
+        Me.cbTwoFactorEnabled.Location = New System.Drawing.Point(46, 243)
+        Me.cbTwoFactorEnabled.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cbTwoFactorEnabled.Name = "cbTwoFactorEnabled"
-        Me.cbTwoFactorEnabled.Size = New System.Drawing.Size(499, 43)
+        Me.cbTwoFactorEnabled.Size = New System.Drawing.Size(320, 29)
         Me.cbTwoFactorEnabled.TabIndex = 0
         Me.cbTwoFactorEnabled.Text = "Two-Factor Authentication"
         Me.cbTwoFactorEnabled.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -50,9 +51,10 @@ Partial Class ProfileEditing
         Me.cbSubscribed.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 10.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cbSubscribed.ForeColor = System.Drawing.Color.Aquamarine
         Me.cbSubscribed.ImageAlign = System.Drawing.ContentAlignment.TopLeft
-        Me.cbSubscribed.Location = New System.Drawing.Point(69, 301)
+        Me.cbSubscribed.Location = New System.Drawing.Point(46, 193)
+        Me.cbSubscribed.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cbSubscribed.Name = "cbSubscribed"
-        Me.cbSubscribed.Size = New System.Drawing.Size(238, 43)
+        Me.cbSubscribed.Size = New System.Drawing.Size(151, 29)
         Me.cbSubscribed.TabIndex = 1
         Me.cbSubscribed.Text = "Subscribed"
         Me.cbSubscribed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -63,9 +65,10 @@ Partial Class ProfileEditing
         Me.btnBack.BackColor = System.Drawing.Color.Aquamarine
         Me.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.btnBack.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnBack.Location = New System.Drawing.Point(12, 598)
+        Me.btnBack.Location = New System.Drawing.Point(8, 383)
+        Me.btnBack.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnBack.Name = "btnBack"
-        Me.btnBack.Size = New System.Drawing.Size(185, 94)
+        Me.btnBack.Size = New System.Drawing.Size(123, 60)
         Me.btnBack.TabIndex = 3
         Me.btnBack.Text = "Back"
         Me.btnBack.UseVisualStyleBackColor = False
@@ -75,9 +78,10 @@ Partial Class ProfileEditing
         Me.btnSave.BackColor = System.Drawing.Color.Aquamarine
         Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup
         Me.btnSave.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 7.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnSave.Location = New System.Drawing.Point(498, 598)
+        Me.btnSave.Location = New System.Drawing.Point(332, 383)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(185, 94)
+        Me.btnSave.Size = New System.Drawing.Size(123, 60)
         Me.btnSave.TabIndex = 4
         Me.btnSave.Text = "Save"
         Me.btnSave.UseVisualStyleBackColor = False
@@ -88,26 +92,29 @@ Partial Class ProfileEditing
         Me.cbDeactivate.Font = New System.Drawing.Font("Gill Sans Ultra Bold", 10.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cbDeactivate.ForeColor = System.Drawing.Color.Aquamarine
         Me.cbDeactivate.ImageAlign = System.Drawing.ContentAlignment.TopLeft
-        Me.cbDeactivate.Location = New System.Drawing.Point(69, 447)
+        Me.cbDeactivate.Location = New System.Drawing.Point(46, 286)
+        Me.cbDeactivate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.cbDeactivate.Name = "cbDeactivate"
-        Me.cbDeactivate.Size = New System.Drawing.Size(378, 43)
+        Me.cbDeactivate.Size = New System.Drawing.Size(242, 29)
         Me.cbDeactivate.TabIndex = 2
         Me.cbDeactivate.Text = "Deactivate Account"
         Me.cbDeactivate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         Me.cbDeactivate.UseVisualStyleBackColor = True
+        Me.cbDeactivate.Visible = False
         '
         'ProfileEditing
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.RoyalBlue
-        Me.ClientSize = New System.Drawing.Size(695, 705)
+        Me.ClientSize = New System.Drawing.Size(463, 451)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.btnBack)
         Me.Controls.Add(Me.cbDeactivate)
         Me.Controls.Add(Me.cbSubscribed)
         Me.Controls.Add(Me.cbTwoFactorEnabled)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "ProfileEditing"
         Me.Text = "ProfileEditing"
         Me.ResumeLayout(False)
--- Pool/ProfileEditing.vb
+++ Pool/ProfileEditing.vb
@@ -1,14 +1,25 @@
 ﻿Public Class ProfileEditing
+
+#Region "Props and InitialSettings"
+    Private Structure InitialSettings
+        Public subscribed As Boolean
+        Public twoFactor As Boolean
+        Public registered As Boolean
+    End Structure
+
+    Dim intialUserSettings As New InitialSettings
+#End Region
+
     Private Sub ProfileEditing_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
         If CurrentSession.IsLoggedIn.Equals(True) Then
             Try
-                cbTwoFactorEnabled.Checked = CurrentSession.TwoFactorEnabled
-                cbSubscribed.Checked = CurrentSession.Subscribed
+                SetCurrentUserSettings()
+                SetIntitialSettings()
             Catch ex As Exception
                 Dim optionsErrorLog As New Logging(Now.ToLongDateString, "Profile Options Error", ex.ToString)
                 optionsErrorLog.LogAction()
             End Try
-
 
         End If
 
@@ -19,8 +30,78 @@
     End Sub
 
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim updateUser As New PlayerStats With {.PID = CurrentSession.ID}
+        Dim sqlString As String = String.Empty
+        Dim results As New DataSet
 
+        'Prompt to selected if nothing was changed
+        If isChanged.Equals(False) Then
+            Dim nothingSelected As DialogResult = MessageBox.Show($"Selected a setting to change!",
+                "No settings selected", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Exit Sub
+        End If
+
+        'Ask again to be safe
+        Dim warningSave As DialogResult = MessageBox.Show($"Are you sure you want to save?",
+                "Saving Settings", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+
+        If warningSave.Equals(DialogResult.Yes) Then
+
+            sqlString = GetUpdateSQL(intialUserSettings)
+            results = updateUser.GetAllResults(sqlString)
+        Else
+            Exit Sub
+        End If
 
         ScoreTheme.LoadNextFormClose(Me, Home)
     End Sub
+
+    ''' <summary>
+    ''' Check if the user changed a value when the form loaded
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function isChanged() As Boolean
+        Dim valuesChanged As Boolean = False
+        If intialUserSettings.registered <> cbDeactivate.Checked Then valuesChanged = True
+        If intialUserSettings.twoFactor <> cbTwoFactorEnabled.Checked Then valuesChanged = True
+        If intialUserSettings.subscribed <> cbSubscribed.Checked Then valuesChanged = True
+        Return valuesChanged
+    End Function
+
+    ''' <summary>
+    ''' Get this sessions settings and set them to controls
+    ''' </summary>
+    Private Sub SetCurrentUserSettings()
+        cbTwoFactorEnabled.Checked = CurrentSession.TwoFactorEnabled
+        cbSubscribed.Checked = CurrentSession.Subscribed
+    End Sub
+
+    ''' <summary>
+    ''' Set inital user's settings on load
+    ''' </summary>
+    Private Sub SetIntitialSettings()
+        intialUserSettings.twoFactor = cbTwoFactorEnabled.Checked
+        intialUserSettings.subscribed = cbSubscribed.Checked
+        intialUserSettings.registered = cbDeactivate.Checked
+    End Sub
+
+    ''' <summary>
+    ''' Builds sp params
+    ''' only pass what has changed
+    ''' </summary>
+    ''' <param name="initialSettings"></param>
+    ''' <returns></returns>
+    Private Function GetUpdateSQL(initialSettings As InitialSettings) As String
+        Dim updUserSettings As New System.Text.StringBuilder
+
+        With updUserSettings
+            .Append("exec updUserSettings ")
+            .Append($"@id={CurrentSession.ID},")
+            If (initialSettings.subscribed <> cbSubscribed.Checked) Then .Append($"@subscribed={Convert.ToInt16(cbSubscribed.Checked)},")
+            If (initialSettings.twoFactor <> cbTwoFactorEnabled.Checked) Then .Append($"@twoFactor={Convert.ToInt16(cbTwoFactorEnabled.Checked)},")
+            If (initialSettings.registered <> cbDeactivate.Checked) Then .Append($"@active={Convert.ToInt16(cbDeactivate.Checked)}")
+        End With
+
+        Return updUserSettings.ToString.TrimEnd(",")
+    End Function
 End Class
